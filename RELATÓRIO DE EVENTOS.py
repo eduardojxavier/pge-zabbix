9d9d423007e04e628193b42ac5e3100411caa286
Eduardo Lira <eduardojxavier@gmail.com>
2024-02-29 14:16:22 -0300

    escrevendo a hora por extenso de duração do evento

--- RELATÓRIO DE EVENTOS.py
+++ RELATÓRIO DE EVENTOS.py
@@ -1,13 +1,13 @@
 from zabbix_api import ZabbixAPI
-import time
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import json
 import pytz
 import pandas as pd
 
-URL = 'http://192.168.5.15/zabbix'
-USERNAME = 'Admin'
-PASSWORD = 'zabbix'
+URL = ''
+USERNAME = ''
+PASSWORD = ''
 
 try:
     zapi = ZabbixAPI(URL, timeout=180)
@@ -18,35 +18,33 @@ except Exception as erro:
     print(f'Não foi possível conectar-se. {erro}')
     exit()
 
+lista_eventos = [] 
+lista_final = [] 
 
-def get_history(): #Função para saber quando a trigger disparou
 
-    dataInicio = 1708947000
-    dataFim = 1708972200
-    reconhecido = False
 
-    lista_eventos = []  #lista para guardar os eventos onde r_eventid não seja 0.
-    lista_reconhecidos = [] 
-    # teste = 136191
+def get_history(lista_eventos, lista_final): 
 
+    dataInicio = 1708999200
+    dataFim = 1709258400
+ 
     fuso_horario_recife = pytz.timezone('America/Recife')
 
-    abertura_evento = zapi.event.get({'output':['eventid','name','host','severity','clock','r_eventid'],
+    abertura_evento = zapi.event.get({'output':['eventid','name','host','severity','clock','r_eventid','acknowledged'],
                                     'selectHosts':['hostid','host'],
                                     'time_from': dataInicio,
                                     'time_till': dataFim,
-                                    'acknowledged': reconhecido})#"eventids": 136191 "eventids": teste' acknowledged': reconhecido
-    
+                                    })   
 
+     
+    
     eventid = 'eventid' 
     name = 'name'
     horaInicioEvento = 'clock'
     r_eventid = 'r_eventid'
     severity = 'severity'
     host = 'host'
-    
-    acknowledged='acknowledged'
-    
+        
     #converter o json em uma lista de dicionários
     converter_json = json.dumps(abertura_evento)
     dicionario_abertura_evento = json.loads(converter_json)
@@ -54,26 +52,34 @@ def get_history(): #Função para saber quando a trigger disparou
     #Testar se o evento está com r_eventid preenchido
     for i in dicionario_abertura_evento:
 
-        id_inicio_evento = i[eventid]
-        severidade_evento = i[severity]
+        #Atribui cada posição do dicionário a uma variável
+        id_inicio_evento = i['eventid']
+        id_retorno_evento = i['r_eventid']
+        nome_evento = i['name']
+        severidade_evento = i['severity']
+        host_evento = i['hosts'][0]['host']
+        eventoReconhecido = i['acknowledged']
 
 
         if (i[r_eventid] != '0') and (i[severity] == '4' or i[severity] == '5'): 
             lista_eventos.append(i)
-            # print (lista_eventos)
+           
+            if severidade_evento == "4":
+                severidade = 'Alta'
+            elif severidade_evento == "5":
+                severidade = 'Desastre'
+
+            if eventoReconhecido == '0':
+                reconhecido = 'Não'
+            else:
+                reconhecido = 'Sim'
             
+
             for j in lista_eventos:
                 converte_hora = int(j['clock'])
                 data_hora = datetime.fromtimestamp(converte_hora,fuso_horario_recife)
-                horaInicio = data_hora.strftime('%d-%m-%Y %H:%M:%S')
-                
-                #Atribui cada posição do dicionário a uma variável
-                id_retorno_evento = j['r_eventid']
-                nome_evento = j['name']
-                severidade_evento = j['severity']
-                host_evento = j['hosts'][0]['host']
-                
-                
+                horaInicio = data_hora.strftime("%Y-%m-%d %H:%M:%S")
+                      
                 
                 fechamento_evento = zapi.event.get({"output":['clock'],
                                                     "eventids": id_retorno_evento})
@@ -83,17 +89,25 @@ def get_history(): #Função para saber quando a trigger disparou
                 for k in dicionario_fechamento_evento:
                     converte_hora_fechamento = int(k['clock'])
                     data_hora_fechamento = datetime.fromtimestamp(converte_hora_fechamento, fuso_horario_recife)
-                    horaFinal = data_hora_fechamento.strftime('%d-%m-%Y %H:%M:%S')
+                    horaFinal = data_hora_fechamento.strftime("%Y-%m-%d %H:%M:%S")
 
-            duracaoEvento = data_hora_fechamento - data_hora        
+            #Converter por extenso o tempo
+            mascara = "%Y-%m-%d %H:%M:%S"
+            ini = datetime.strptime(horaInicio, mascara)
+            fim = datetime.strptime(horaFinal, mascara)
+
+            di = abs(relativedelta(ini, fim))
+
+            tempoEvento = (f'{di.days} dias, {di.hours} horas, {di.minutes} minutos')
+
+            resultado = id_inicio_evento, nome_evento, host_evento, severidade, horaInicio, horaFinal, tempoEvento, reconhecido
+                 
+            # lista_final.append(resultado)
+            # print(lista_final)
+            print(resultado)
 
 
-            # print(acknowledged)
-            print(id_inicio_evento, nome_evento, host_evento, severidade_evento, horaInicio, horaFinal, duracaoEvento, acknowledged) 
-            # print(host_evento, nome_evento, severidade_evento, horaInicio, horaFinal, duracaoEvento)      
 
 
 
-
-
-print(get_history())
+print(get_history(lista_eventos,lista_final))
